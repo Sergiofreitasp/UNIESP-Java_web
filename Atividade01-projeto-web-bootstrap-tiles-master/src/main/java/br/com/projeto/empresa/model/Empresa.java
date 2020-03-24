9fc26cbd0a4442744a5362a2d9734ed3e67bed0f
Sérgio <55296032+Sergiofreitasp@users.noreply.github.com>
2020-03-24 11:09:13 -0300

    Add files via upload
    Atividade 01

---
+++ Atividade01-projeto-web-bootstrap-tiles-master/src/main/java/br/com/projeto/empresa/model/Empresa.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package br.com.projeto.empresa.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author carlos
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class Empresa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	
+	private String email;
+	
+	private String cnpj;
+	
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
